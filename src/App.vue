bfb1d97da958a70d72eeb18c9d5e2ba704432b02
BaisiTech <123344359+BaisiTech@users.noreply.github.com>
2023-08-23 00:14:41 +0800

    初现雏形

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,33 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <RouterLink to="/">主页</RouterLink>&nbsp;&nbsp;
+    <RouterLink to="/about">商店</RouterLink>
+    <span class="top_Right">dd</span>
   </header>
 
-  <RouterView />
+  <RouterView style="height: calc(100% - 3.4rem);"/>
 </template>
 
 <style scoped>
+  header{
+    background: linear-gradient(45deg,#f55fa7,#55acf9);
+    padding: 1rem;
+  }
 
+  a{
+    text-decoration: none;
+    color: white;
+  }
+
+  .top_Right{
+    float: right;
+    color: white;
+  }
+
+  .router-link-active{
+    /* text-decoration: white wavy underline; */
+    padding-bottom: 2px;
+    border-bottom:2px solid white
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
-<script setup>
+<script>
 </script>
 
 <template>
-  <main>
-    <p>welcome</p>
-  </main>
+  <div class="total">
+    <p class="theTitle"><br>欢迎来到 BaisiMC !</p>
+  </div>
 </template>
+
+<style scoped>
+  .total{
+    background: url("/src/assets/minecraft_home.webp") no-repeat center center;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+  .theTitle{
+    font-size: 5rem;
+    margin: 0;
+    text-align: center;
+    color: white;
+  }
+</style>
